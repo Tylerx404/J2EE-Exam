8ae5a63d97d26afa76fb8286c1674c2c0370855b
Đinh Vạn Tài <vantaidinh04@hotmail.com>
2026-03-24 13:26:18 +0700

    Initial commit

---
+++ src/main/java/com/exam/taidinh/J2EE_Exam/J2EeExamApplication.java
@@ -0,0 +1,13 @@
+package com.exam.taidinh.J2EE_Exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2EeExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2EeExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/taidinh/J2EE_Exam/J2EeExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.taidinh.J2EE_Exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2EeExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
